3b27a981d524ac148b8b60e3eec8a3a41ac3c98f
StefansArya <stefansarya1@gmail.com>
2022-03-15 09:21:57 +0700

    Update configurations

---
+++ tests/browser.test.js
@@ -0,0 +1,17 @@
+require("@blackprint/engine");
+
+// === For Browser Environment ===
+window.sf = require("scarletsframe/dist/scarletsframe.min.js");
+window.ResizeObserver = class{};
+require("@blackprint/sketch/dist/blackprint.min.js");
+require("@blackprint/sketch/dist/blackprint.sf.js");
+// === For Browser Environment ===
+
+test('Blackprint.Sketch does exist on window', () => {
+  expect(window.Blackprint.Sketch).toBeDefined();
+});
+
+// test('Load custom node modules', () => {
+// 	require('../dist/nodes-rename-me.mjs');
+// 	require('../dist/nodes-rename-me.sf.mjs');
+// });
